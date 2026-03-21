1f2af7b9c5379b5c46dfc9f39422c7caa0252760
Ricardo A. Sanjur G. <ricardosanjurg@gmail.com>
2026-03-21 18:44:22 -0500

    arreglamos la configuracion

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    imageService: 'cloudflare',
-    compatibilityFlags: ['nodejs_compat_v2']
-  }),
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [react(), svelte()],
 
   vite: {
